3b8f5ca4b2af8e626020e17951bf24191430e28d
Abhishek Mishra <abhi@Abhisheks-MacBook-Air.local>
2026-02-26 00:04:20 +0530

    added deployment config

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:5001/api' })
+const api = axios.create({ baseURL: 'https://disaster-relief-backend-x5uo.onrender.com/api' })
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
